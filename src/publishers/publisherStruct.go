41e4aa7fc7c685c9bc4a53d6d1266068b7af4ca8
LeGrandAlan <alan.le-grand@protonmail.com>
2020-10-06 15:22:10 +0200

    [UPDATE] Sending captors data from multiple airports

--- src/publishers/publisherStruct.go
+++ src/publishers/publisherStruct.go
@@ -5,36 +5,30 @@ import (
 )
 
 type Publisher struct {
-
-	Id        string
+	Id        int
 	AirportId string
-	Name      string
 	Type      string
 	Unit      string
 	Min       float64
 	Max       float64
-	Topic     string
-
 }
 
 func (o Publisher) String() string {
 
-	return fmt.Sprintf("{ Id: %s, AirportId: %s, Name: %s, Type: %s, Unit: %s, Min: %.2f, Max: %.2f, Topic: %s }",
-		o.Id, o.AirportId, o.Name, o.Type, o.Unit, o.Min, o.Max, o.Topic)
+	return fmt.Sprintf("{ Id: %d, AirportId: %s, Type: %s, Unit: %s, Min: %.2f, Max: %.2f }",
+		o.Id, o.AirportId, o.Type, o.Unit, o.Min, o.Max)
 
 }
 
 func MakeFromMap(m map[string]interface{}) Publisher {
 
 	res := Publisher{
-		Id:        m["Id"].(string),
+		Id:        int(m["Id"].(float64)),
 		AirportId: m["AirportId"].(string),
-		Name:      m["Name"].(string),
 		Type:      m["Type"].(string),
 		Unit:      m["Unit"].(string),
 		Min:       m["Min"].(float64),
 		Max:       m["Max"].(float64),
-		Topic:     m["Topic"].(string),
 	}
 	return res
 
